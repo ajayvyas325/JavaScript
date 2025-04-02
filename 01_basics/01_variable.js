38040622023e967b8d6b9da62a63b90dae78f55e
ajayvyas325 <82799310+ajayvyas325@users.noreply.github.com>
2025-04-02 05:27:17 +0000

    data type

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accntId = 1234 // const value is fixed. 
+let accntGmail = "ajay@gmail.com" // in let variable can be change the value. but given scope
+var accntPasswd = "12345" //in let variable can be change the value. but, in all scope
+accntCity = "Jaipur"
+
+accntGmail="av@gmail.com"
+accntPasswd="1452"
+accntCity="Pune"
+
+console.table([accntId, accntGmail, accntPasswd, accntCity])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,31 @@
+"use strict" // treat all JS code as new version
+let x = 3.14
+console.log(x);
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x1 = true;
+let y1 = false;
+
+/* 
+Data Type-
+Primitive Data Type - 
+1. Number - stored the numeric and floating value
+2. String - stored the character and string value
+3. boolean - true/ false
+4. bigInt - stored the big Int value
+5. null - standalone value
+6. undefined - stored the undefined value
+7. symbol - unique
+
+
+Non-Primitive Data Type-
+1. Array - is stored the simailar data type;
+2. object 
+3. Map
+*/
